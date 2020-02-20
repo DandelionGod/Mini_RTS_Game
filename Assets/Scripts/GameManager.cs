69a1ef5ae23db41fd8568f78eb2b7d12c365b6a2
zubenko kirill <kirill.zubenko.neitron@gmail.com>
2020-02-21 00:32:16 +0200

    Kirills example

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,43 @@
+﻿using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private TextMeshProUGUI _populationView;
+
+	private int _population = 200;
+
+	private const float _GS = 1.0f;
+	private const int _POPULATION_LIMIT = 1000;
+	private const int _POPULATION_PLUS = 100;
+
+
+	private float _gs;
+
+	
+
+    void Start()
+    {
+	    Debug.Log("GameStart");
+    }
+
+
+    void Update()
+    {
+	    if (_gs >= _GS)
+	    {
+		    if (_population + _POPULATION_PLUS <= _POPULATION_LIMIT)
+			    _population += _POPULATION_PLUS;
+		    else
+			    _population += _POPULATION_LIMIT - _population;
+
+		    _gs = 0.0f;
+	    }
+
+	    _populationView.text = $"{_population}";
+
+	    _gs += Time.deltaTime;
+    }
+
+
+}
