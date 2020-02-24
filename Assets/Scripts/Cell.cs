5fdc1af11bc93a15073119dd7043e5cbd1bac51f
Dandelion_God <dandeliongod95@gmail.com>
2020-02-25 00:37:03 +0200

    Cell : Map, added MapGenerator

--- Assets/Scripts/Cell.cs
+++ Assets/Scripts/Cell.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Cell : MonoBehaviour
+public class Cell : Map
 {
     private MeshRenderer _mr;
 
---
+++ Assets/Scripts/Map.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Map : MonoBehaviour
+{
+	[SerializeField] private List<Map> _mapsCells = new List<Map>();
+	
+	[SerializeField] private Map _originalCell;
+	[SerializeField] private GameObject _mapCellsParent;
+
+
+	void Start()
+	{
+		MapGenerator(15, 15);
+	}
+
+
+	void Update()
+	{
+
+	}
+
+
+	public void MapGenerator(int n, int m)
+	{
+		var cell = new Vector3[n, m];
+		
+		float s = 1;
+
+		for (int j = 0; j < m; j++)
+		{
+			for (int i = 0; i < n; i++)
+			{
+				cell[i, j].x = i * s;
+				cell[i, j].y = j * s;
+
+				_mapsCells.Add(Instantiate(_originalCell, _mapCellsParent.transform));
+				Debug.Log($"cell[{cell[i, j].x}, {cell[i, j].y}]");
+			}
+		}
+	}
+}
